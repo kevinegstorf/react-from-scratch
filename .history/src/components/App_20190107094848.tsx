87eb06833a59eb8e8882b8d92a8810e88e69bf74
kevinegstorf <kevin.egstorf@gmail.com>
2019-01-08 14:14:44 +0100

    first commit

---
+++ .history/src/components/App_20190107094848.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export interface IAppProps {}
+
+export function IApp(props: IAppProps) {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+}
---
+++ .history/webpack.config_20190107110318.ts
@@ -0,0 +1,28 @@
+import * as path from "path";
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+
+  module: {
+    rules: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader" }
+    ]
+  },
+  plugins: [htmlPlugin]
+};
